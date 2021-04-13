22efc7cc5c6bb9ebe44672d45485e61a8fec6b06
TJ Hindman <thind131@gmail.com>
2021-04-12 23:27:57 -0400

    basic login/register form set up

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export const SignUp = () => {
+  const [state, setState] = useState({
+    username: "",
+    displayName: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setState((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+    setState({
+      username: "",
+      displayName: "",
+      password: "",
+    });
+  };
+
+  return (
+    <Card style={{ display: "flex", width: "40vw" }}>
+      <Card.Body style={{ display: "flex", flexDirection: "column" }}>
+        <Card.Title as="h1" style={{ alignSelf: "center" }}>
+          Sign Up
+        </Card.Title>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              name="username"
+              value={state.username}
+              onChange={handleChange}
+              placeholder="Enter username"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicDisplayName">
+            <Form.Label>Display Name</Form.Label>
+            <Form.Control
+              name="displayName"
+              value={state.displayName}
+              onChange={handleChange}
+              placeholder="Enter display name"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={state.password}
+              onChange={handleChange}
+              placeholder="Enter password"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your password with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button type="submit">Sign Up</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
---
+++ src/views/AuthUser/AuthUser.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+
+import { Login, SignUp } from "../../components";
+
+export const AuthUser = (props) => {
+  const [userStatus, setUserStatus] = useState("login");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "8vh",
+      }}
+    >
+      {userStatus === "login" ? (
+        <>
+          <Login />
+          <div style={{ paddingTop: "5vh" }}>
+            New to Kwitter? 
+            <Button onClick={() => setUserStatus("signUp")}>Sign up</Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <SignUp />
+          <div style={{ paddingTop: "5vh" }}>
+            Already have an account? 
+            <Button onClick={() => setUserStatus("login")}>Login</Button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
